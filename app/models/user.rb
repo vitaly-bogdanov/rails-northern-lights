f8c48a51f4a89f0dcdc5e067bbe4ed8957a577dd
kaliummati <kaliummati@gmail.com>
2019-11-19 21:06:44 +0900

    множество поправок

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :rememberable, :validatable
+
+  devise :database_authenticatable, :rememberable, :timeoutable, :trackable, :validatable
 end
