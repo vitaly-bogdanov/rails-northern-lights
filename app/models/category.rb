547713dcd836f4d1537b7f67ebcfe45ad0e5c3e4
kaliummati <kaliummati@gmail.com>
2019-11-20 23:12:10 +0900

    сделан вывод валидационных ошибок при помощи хелперов

--- app/models/category.rb
+++ app/models/category.rb
@@ -8,8 +8,11 @@
 class Category < ApplicationRecord
   has_many :products, dependent: :destroy
 
+  NAME_MAX_LENGTH = 12
+
   # обязательные поля
-  validates :name,        presence: { message: 'Название категории обязательно' }
+  validates :name,        presence: { message: 'Название категории обязательно' },
+                          length:   { maximum: NAME_MAX_LENGTH, message: 'Законы геометрии не позволяют втеснить такое название' }
   validates :description, presence: { message: 'Описание категории обязательно' }
   validates :keywords,    presence: { message: 'Парочка ключевых слов пригодятся для для SEO оптимизации' }
 end
