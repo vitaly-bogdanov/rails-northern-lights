0de33f3e4abbd432ce52138656ed2e6e1a990083
kaliummati <kaliummati@gmail.com>
2019-12-21 10:57:55 +0900

    добавлениие aws s3

--- config/initializers/aws.rb
+++
@@ -1,6 +0,0 @@
-Aws.config.update({
-  region: 'ap-northeast-2',
-  credentials: Aws::Credentials.new(Rails.application.credentials.dig(:aws, :access_key_id), Rails.application.credentials.dig(:aws, :secret_access_key))
-})
-  
-S3_BUCKET = Aws::S3::Resource.new.bucket('severnoe-siyanie')
