a7ffbdc90dc54e078ccd2e2eebbe862f5eb2634b
Vitaly <kaliummati@gmail.com>
2020-06-10 00:25:28 +0900

    уборка в коде моделей

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,25 +6,32 @@
   Имеет поля: name, description, keywords.
 =end
 class Category < ApplicationRecord
-    # friendly url
+  # связь "один ко многим"
+  has_many :products, dependent: :destroy
+
+  # настройки slug
   extend FriendlyId
   friendly_id :name, use: :slugged
   def normalize_friendly_id(input)
     input.to_s.to_slug.normalize(transliterations: :russian).to_s
   end
+
   def should_generate_new_friendly_id?
     name_changed?
   end
 
-  has_many :products, dependent: :destroy
-
-  NAME_MAX_LENGTH = 12
+  NAME_MAX_LENGTH = 12 # максимальная длинна названия категории
 
   # обязательные поля
-  validates :name,        presence: { message: 'Название категории обязательно' },
-                          length:   { maximum: NAME_MAX_LENGTH, message: 'Законы геометрии не позволяют втеснить такое название' }
-  validates :description, presence: { message: 'Описание категории обязательно' }
-  validates :keywords,    presence: { message: 'Парочка ключевых слов пригодятся для для SEO оптимизации' }
+  validates :name,
+    presence: { message: 'Название категории обязательно' },
+    length: { maximum: NAME_MAX_LENGTH, message: 'Законы геометрии не позволяют втеснить такое название' }
+
+  validates :description,
+    presence: { message: 'Описание категории обязательно' }
+
+  validates :keywords,
+    presence: { message: 'Парочка ключевых слов пригодятся для для SEO оптимизации' }
 
   def name_max_length
     NAME_MAX_LENGTH
--- app/models/order_call.rb
+++ app/models/order_call.rb
@@ -1,11 +1,26 @@
+=begin
+  Заказ уникального товара
+=end
+
 class OrderCall < ApplicationRecord
   belongs_to :product
 
-  validates :name,      presence: { message: 'Укажите ваше имя' }
-  validates :telephone, presence: { message: 'Укажите ваш телефон' },
-                          format: {
-                            with: /(\+7|8)[- _]*\(?[- _]*(\d{3}[- _]*\)?([- _]*\d){7}|\d\d[- _]*\d\d[- _]*\)?([- _]*\d){6})/, 
-                            message: 'Не корректный номер' 
-                          }
-  validates :confirm, acceptance: { allow_nil: false, message: 'Необходимо принять политику приватности' }
+  # валидация имени
+  validates :name,
+    presence: { message: 'Укажите ваше имя' }
+
+  # валидация телефонного номера
+  validates :telephone,
+    presence: { message: 'Укажите ваш телефон' },
+    format: {
+      with: /(\+7|8)[- _]*\(?[- _]*(\d{3}[- _]*\)?([- _]*\d){7}|\d\d[- _]*\d\d[- _]*\)?([- _]*\d){6})/,
+      message: 'Не корректный номер'
+    }
+
+  # валидация согласия с политикой приватности
+  validates :confirm,
+    acceptance: {
+      allow_nil: false,
+      message: 'Необходимо принять политику приватности'
+    }
 end
