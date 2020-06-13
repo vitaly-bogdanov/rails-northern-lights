c36e1afc75023981125c0995af0560eb26eb767d
Vitaly <kaliummati@gmail.com>
2020-06-14 03:02:32 +0900

    небольшие правки

--- db/migrate/20190910131144_create_products.rb
+++ db/migrate/20190910131144_create_products.rb
@@ -1,16 +1,18 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.belongs_to :category
-      t.string     :picture
-      t.string     :name
-      t.integer    :price
-      t.text       :description
-      t.string     :preview
-      t.text       :keywords
-      t.boolean    :available, default: true
-      t.boolean    :unique
+      t.belongs_to :category, null: false
+      t.string :picture, null: true
+      t.string :name, null: false
+      t.integer :price, null: false
+      t.text :description, null: false
+      t.string :preview, null: false
+      t.text :keywords, null: false
+      t.boolean :available, default: true
+      t.boolean :unique
       t.timestamps
     end
+
+    add_index :products, :name # для ускорения поиска
   end
 end
