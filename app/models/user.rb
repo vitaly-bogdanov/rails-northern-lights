954b75007ede4d8a163cc30e2deb2e0fed575339
Vitaly <kaliummati@gmail.com>
2020-06-14 03:31:52 +0900

    удаление лишней модели

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  devise :database_authenticatable,
-         :rememberable,
-         :timeoutable,
-         :validatable
+  devise(
+    :database_authenticatable,
+    :rememberable,
+    :timeoutable,
+    :validatable
+  )
 end
