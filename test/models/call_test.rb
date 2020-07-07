21393397f04581cf1f3429e9270dec882d58b75f
Vitaly <kaliummati@gmail.com>
2020-07-08 01:31:45 +0900

    добавлен тест модели call

--- test/models/call_test.rb
+++ test/models/call_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class CallTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'попытка создать запись без указания имени' do
+    assert_not build(:call, :void_name).valid?
+  end
+  test 'попытка создать запись без указания телефона' do
+    assert_not build(:call, :void_telephone).valid?
+  end
+  test 'попытка создать запись с невалидным мобильным номером РФ' do
+    assert_not build(:call, :invalid_telephone).valid?
+  end
+  test 'попытка создать запись без поля confirm' do
+    assert_not build(:call, :not_confirm).valid?
+  end
+  test 'создание записи при том, что все поля переданны согласно правилам валидации' do
+    assert build(:call, :valid_all).valid?
+  end
 end
