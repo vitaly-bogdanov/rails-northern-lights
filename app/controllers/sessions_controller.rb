e5ffc7908859d28254ab5adedf3ab2a04b294b30
Vitaly <kaliummati@gmail.com>
2020-07-07 12:01:04 +0900

    исправлен редирект после авторизации

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,9 @@
 class SessionsController < Devise::SessionsController
   layout 'authentication'
+
+  after_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || admin_calls_path
+  end
 end
