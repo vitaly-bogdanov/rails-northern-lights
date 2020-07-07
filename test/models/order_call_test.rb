719a49847a71e221d0540651af35430f533c2a31
Vitaly <kaliummati@gmail.com>
2020-07-08 03:43:13 +0900

    добавлен тест для модели order_call

--- test/models/order_call_test.rb
+++ test/models/order_call_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class OrderCallTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'попытка создать запись оставив поле name пустым' do
+    assert_not build(:order_call, :new_order_call, name: '').valid?
+  end
+  test 'попытка создать запись с невалидным номером телефона' do
+    assert_not build(:order_call, :new_order_call, telephone: '89j8hk98j8').valid?
+  end
+  test 'попытка создать запись без заполнения поля confirm' do
+    assert_not build(:order_call, :new_order_call, confirm: nil).valid?
+  end
+  test 'попытка создать запись при заполнении всех полей согласно правилам валидации' do
+    assert build(:order_call, :new_order_call).valid?
+  end
 end
