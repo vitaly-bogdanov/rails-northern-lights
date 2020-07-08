0d8feef6ff4f3827db3afec111189a0be23995ac
Vitaly <kaliummati@gmail.com>
2020-07-09 00:28:03 +0900

    создан тест для модели order

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'попытка создать заказ' do
+    assert build(:order, :new_order).valid?
+  end
 end
