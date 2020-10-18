f9811dfef0bb610a95283b8269e87d53fa211d14
Vitaly <kaliummati@gmail.com>
2020-10-19 03:03:53 +0900

    исправлен баг, при котором содавалась пустая запись в бд

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,24 +5,22 @@ class Contact < ApplicationRecord
       if constact_info
         constact_info
       else
+        self.cache_clear
         self.create
       end
     end
+    def cache_clear
+      Rails.cache.delete('contacts')
+      Rails.cache.delete('phone_call')
+    end
   end
-
   def call_format_phone
     Rails.cache.fetch('phone_call') do
       self.phone.each_char.select { |item| item == '0' or item.to_i != 0 }.join
     end
   end
-
   def update_info params
     self.update_attributes params # обновляем контакты
-    self.cache_clear # чистим кеш
-  end
-  
-  def cache_clear
-    Rails.cache.delete('contacts')
-    Rails.cache.delete('phone_call')
+    self.class.cache_clear # чистим кеш
   end
 end
