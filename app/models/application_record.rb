d5d69f74b06082ecd4405313cb8f6db3d5b40b28
Vitaly <kaliummati@gmail.com>
2020-10-28 15:18:48 +0900

    картинка обязательна для загрузки

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,6 +3,8 @@ class ApplicationRecord < ActiveRecord::Base
   def validate_picture
     if picture.attached?
       errors.add(:picture, 'Изображение должно иметь формат jpeg или jpg') unless picture.content_type.in?(%w(image/jpeg image/jpg))
+    else
+      errors.add(:picture, 'Изображение обязательно')
     end
   end
 end
