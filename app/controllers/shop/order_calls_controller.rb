61ff1be6731569614d2f8b224b498999f35aec49
Vitaly <kaliummati@gmail.com>
2020-10-28 15:53:30 +0900

    исправленна ошибка с именами в полях формы

--- app/controllers/shop/order_calls_controller.rb
+++ app/controllers/shop/order_calls_controller.rb
@@ -1,6 +1,7 @@
 class Shop::OrderCallsController < ApplicationController
   def create
     @order_call = OrderCall.new(order_call_params)
+    puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
     if @order_call.save
       render json: { controller: 'order_call#create' }, status: :created
     else
@@ -12,11 +13,11 @@ class Shop::OrderCallsController < ApplicationController
 
   def order_call_params
     params.permit(
-      :name_order_call,
-      :telephone_order_call,
-      :confirm_order_call,
-      :product_id_order_call,
-      :timezone_order_call,
+      :name,
+      :telephone,
+      :confirm,
+      :product_id,
+      :timezone,
       :product_name,
       :product_price
     )
